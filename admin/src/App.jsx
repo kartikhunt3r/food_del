4854c026a2c60f1b908386cd96b49d5b45be34fb
Kartik Savaliya <110083841+kartikhunt3r@users.noreply.github.com>
2024-07-18 11:34:06 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://food-del-backend-e3ra.onrender.com/";
+  const url = "https://food-del-backend-e3ra.onrender.com";
   return (
     <div>
       <ToastContainer />
